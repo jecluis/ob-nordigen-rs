ff124cde9a8c160a1adb5faf26f8720a38f0df87
Joao Eduardo Luis <joao@abysmo.io>
2023-01-02 10:45:20 -0100

    nordigen: drop file-based functions for state class
    Signed-off-by: Joao Eduardo Luis <joao@abysmo.io>

--- src/state.rs
+++ src/state.rs
@@ -22,86 +22,33 @@ pub struct NordigenState {
     pub token_expires: u32,
     pub refresh_token: String,
     pub refresh_expires: u32,
-    written_at: DateTime<Utc>,
+    pub last_updated: DateTime<Utc>,
 }
 
 impl NordigenState {
-    pub fn parse(path: &std::path::PathBuf) -> Result<Self, String> {
-        if !path.exists() {
-            return Err(format!(
-                "State file at {} does not exist!",
-                path.display()
-            ));
-        }
-
-        let contents = match std::fs::read_to_string(path) {
-            Err(error) => {
-                return Err(format!(
-                    "Error reading file at {}: {}",
-                    path.display(),
-                    error
-                ));
-            }
-            Ok(value) => value,
-        };
-
-        let state: NordigenState = match serde_json::from_str(&contents) {
-            Err(error) => {
-                return Err(format!(
-                    "Unable to parse state file at {}: {}",
-                    path.display(),
-                    error
-                ))
-            }
-            Ok(value) => value,
-        };
-
-        Ok(state)
-    }
-
-    pub fn write(
-        path: &std::path::PathBuf,
+    pub fn new(
         token: String,
-        refresh: String,
-        token_ttl: u32,
-        refresh_ttl: u32,
-    ) -> Result<Self, String> {
-        let state: NordigenState = NordigenState {
+        token_expires: u32,
+        refresh_token: String,
+        refresh_expires: u32,
+    ) -> NordigenState {
+        NordigenState {
             token,
-            token_expires: token_ttl,
-            refresh_token: refresh,
-            refresh_expires: refresh_ttl,
-            written_at: Utc::now(),
-        };
-
-        let buffer = match std::fs::File::create(path) {
-            Err(err) => {
-                return Err(format!(
-                    "Unable to open state file for writing: {}",
-                    err
-                ));
-            }
-            Ok(res) => res,
-        };
-
-        match serde_json::to_writer_pretty(buffer, &state) {
-            Err(err) => {
-                return Err(format!("Unable to write state to disk: {}", err));
-            }
-            Ok(_) => {}
-        };
-
-        Ok(state)
+            token_expires,
+            refresh_token,
+            refresh_expires,
+            last_updated: Utc::now(),
+        }
     }
 
     pub fn token_expires_on(&self) -> DateTime<Utc> {
-        self.written_at
+        self.last_updated
             .checked_add_signed(Duration::seconds(self.token_expires.into()))
             .expect("Unable to obtain end date!")
     }
 
     pub fn refresh_expires_on(&self) -> DateTime<Utc> {
-        self.written_at
+        self.last_updated
             .checked_add_signed(Duration::seconds(self.refresh_expires.into()))
             .expect("Unable to obtain end date!")
     }
